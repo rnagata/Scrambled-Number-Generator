763e4bfe02e8ed71663faf9f412d19193fbc7c86
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-23 09:14:40 -1000

    First Solution

--- generator.js
+++ generator.js
@@ -1,3 +1,19 @@
+'use strict';
+
 module.exports = function(n) {
   // do work here
+  let uniqueNumbers = []
+  let randomArray = [];
+
+  for (let i = 0; i < n; i++){
+    uniqueNumbers.push(i);
+  }
+
+  while (uniqueNumbers.length > 0){
+    let index = Math.floor(Math.random() * uniqueNumbers.length);
+    let removed = uniqueNumbers.splice(index, 1);
+    randomArray.push(removed);
+  }
+  
+  return randomArray;
 };
